4f239c4ff37e9e571acef9f3b16af4745871883a
bima-taruna <bimataruna21@gmail.com>
2024-12-09 11:57:20 +0700

    final commit

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 import HashMap from "./hashmap.js";
-let hashmap = new HashMap();
-hashmap.set("apple", "red");
-hashmap.set("banana", "yellow");
-hashmap.set("durian", "green");
-hashmap.remove("durian");
-console.log(hashmap.length());
-console.log(hashmap.printLoadFactor());
-console.log(hashmap.entries());
+const test = new HashMap();
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
+test.set("moon", "silver");
+
+console.log(test.entries());
